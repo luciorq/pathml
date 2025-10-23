5145353d0edd929ea003aba6f18ac7a4985adf5d
jamesgwen <jgw787@gmail.com>
2025-10-23 10:29:03 -0700

    changing format of test script

--- tests/test_manuscript_urls.py
+++ tests/test_manuscript_urls.py
@@ -6,30 +6,27 @@ License: GNU GPL 2.0
 import urllib.request
 import pytest
 
+
 @pytest.mark.parametrize(
     "url",
     [
         "https://www.pathml.org",
         # Vignettes
         "https://github.com/Dana-Farber-AIOS/pathml/tree/master/examples/vignettes/",
-        # docs
+        # Docs
         "https://pathml.readthedocs.io/en/latest/",
     ],
 )
-
 def test_urls(url):
     """
     Make sure that the URLs linked in the manuscript are not broken.
     Adds a User-Agent header to avoid 403 errors from some servers.
     """
-    req = urllib.request.Request(
-        url,
-        headers={"User-Agent": "Mozilla/5.0"}  # mimic a browser
-    )
+    req = urllib.request.Request(url, headers={"User-Agent": "Mozilla/5.0"})
     try:
-        r = urllib.request.urlopen(req)
+        response = urllib.request.urlopen(req)
         # HTTP status code 200 means "OK"
-        assert r.getcode() == 200
+        assert response.getcode() == 200
     except urllib.error.HTTPError as e:
         # If site blocks CI (e.g., 403), skip the test instead of failing
         if e.code == 403:
